a91ef37534db567b427e1cb3effa1d7613b7fddf
wheelhouseclassics <info@wheelhouseclassics.com>
2025-10-26 21:01:17 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+header("Content-Type: application/json");
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+$api_key = "napi_tdic91bhx55200pko5ukwpb09qntqz5vzzk56cnzv23913o6j9elfyqsxqusnlyr";
+$sql = "SELECT make, model, year, payment FROM lease_programs LIMIT 10;";
+$body = json_encode(["sql" => $sql]);
+
+$url = "https://api.neon.tech/v2/projects/bitter-scene-77398435/branches/br-red-sea-adw4083a/query";
+
+$ch = curl_init($url);
+curl_setopt_array($ch, [
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_HTTPHEADER => [
+    "Authorization: Bearer $api_key",
+    "Content-Type: application/json"
+  ],
+  CURLOPT_POST => true,
+  CURLOPT_POSTFIELDS => $body
+]);
+
+$response = curl_exec($ch);
+$http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+$err = curl_error($ch);
+curl_close($ch);
+
+if ($err) {
+  echo json_encode(["error" => $err]);
+  exit;
+}
+
+echo json_encode([
+  "http_status" => $http_code,
+  "response" => json_decode($response, true)
+], JSON_PRETTY_PRINT);
+?>
+
