94a6feffbb33df4b8efdbe8d684bcd2cbaf3d085
wheelhouseclassics <info@wheelhouseclassics.com>
2025-10-27 15:04:26 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+header("Content-Type: application/json");
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Origin: *");        // <-- critical for your Bluehost front-end
 header("Access-Control-Allow-Methods: GET, OPTIONS");
